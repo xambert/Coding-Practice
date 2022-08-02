998fba838fbd977a393511fc653f85336899e035
Priyanshu Sharma <50556703+xambert@users.noreply.github.com>
2022-08-03 04:29:53 +0530

    Create goodNodes_1.cpp

---
+++ Codes/goodNodes_1.cpp
@@ -0,0 +1,10 @@
+int isGood(TreeNode* root, int curr_max)
+{
+if(root==NULL) return 0;
+curr_max = max(root->val, curr_max);
+return (root->val == curr_max?1:0) + isGood(root->left, curr_max) + isGood(root->right, curr_max);
+}
+ int goodNodes(TreeNode* root) {
+int curr_max = root->val;
+return isGood(root, curr_max);
+    }
