13b83a6dc0578737be9125aa5e06caf93f4e2c8d
Priyanshu Sharma <50556703+xambert@users.noreply.github.com>
2022-08-08 02:23:25 +0530

    Create checkInclusion_opt.cpp

---
+++ Codes/checkInclusion_opt.cpp
@@ -0,0 +1,23 @@
+bool checkInclusion(string s1, string s2) {
+        if(s1.length() > s2.length()) return false;
+        vector<int> freq(26, 0);
+        for(char ch: s1) freq[ch-'a']++;
+        int ws = s1.size(), i;
+        int covered = ws;
+        for(i=0 ;i< ws;i++)
+        {
+            //if in s1, freq>0
+            if(freq[s2[i]-'a']-- >0) covered--;
+        }
+        if(covered==0) return true;
+        //not found in curr window
+        for(i=1;i<=s2.length()-ws;i++)
+        {
+            //if in s1 and in prev window, freq of s2[i-1] >=0
+            if(freq[s2[i-1]-'a']++ >= 0) covered++; // cases ["ba", "boa"] ["adc", "adac"] 
+            //if in s1 and in curr_window, 
+            if(freq[s2[i+ws-1]-'a']-- > 0) covered --;
+            if(covered==0) return true;
+        }
+        return false;
+    }
